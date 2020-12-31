1414b4495dee2befbbf26c87ce47fa703c452fc9
Edward <muhedwardc@gmail.com>
2020-12-31 20:39:48 +0700

    get user input

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDecorator: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjustedDecorator;
+}
+
 class ProjectInput {
     templateEl: HTMLTemplateElement;
     hostEl: HTMLDivElement;
@@ -14,9 +26,9 @@ class ProjectInput {
         this.formEl = <HTMLFormElement>importedContentNode.firstElementChild;
         this.formEl.id = 'user-input';
 
-        this.titleInputEl = <HTMLInputElement>this.formEl.getElementById('title')!
-        this.descriptionInputEl = <HTMLTextAreaElement>this.formEl.getElementById('description')!
-        this.peopleInputEl = <HTMLInputElement>this.formEl.getElementById('people')!
+        this.titleInputEl = <HTMLInputElement>this.formEl.querySelector('#title')!
+        this.descriptionInputEl = <HTMLTextAreaElement>this.formEl.querySelector('#description')!
+        this.peopleInputEl = <HTMLInputElement>this.formEl.querySelector('#people')!
 
         this.configure();
         this.attach();
@@ -27,11 +39,38 @@ class ProjectInput {
     }
 
     private configure() {
-        this.formEl.addEventListener('submit', this.submitHandler.bind(this));
+        this.formEl.addEventListener('submit', this.submitHandler);
     }
 
+    private getUserInput(): [string, string, number] | void {
+        const title = this.titleInputEl.value;
+        const desc = this.descriptionInputEl.value;
+        const people = this.peopleInputEl.value;
+
+        if (title.trim().length <= 0 || desc.trim().length <= 0 || parseInt(people.trim()) <= 0) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [title, desc, +people];
+        }
+    }
+
+    private resetForm() {
+        this.titleInputEl.value = '';
+        this.descriptionInputEl.value = '';
+        this.peopleInputEl.value = '';
+    }
+
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
+
+        const userInput = this.getUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log({title, desc, people});
+            this.resetForm();
+        }
     }
 }
 
